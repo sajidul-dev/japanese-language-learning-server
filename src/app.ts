c6c028f443449509e7ba74e02fd742130df8832b
sajidul-dev <mdsajidulalam1245@gmail.com>
2024-12-11 14:00:38 +0600

    [Done]: Cors policy added

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,7 @@ const app: Application = express();
 app.use(
   cors({
     origin: "*",
-    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   }),
 );
 app.use(cookieParser());
